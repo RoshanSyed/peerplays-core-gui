d33ccf02f6380c4900b2130499d29369de4a3f7b
Roshan Syed <r.syed@pbsa.info>
2019-03-25 10:02:14 -0300

    Added CI/CD through GitLab and AWS

---
+++ deploy/updateAndRestart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker stop peerplays-core-gui || true
+docker rm peerplays-core-gui || true
+docker build -t peerplays/core-gui .
+docker run -p 8082:8082 --name peerplays-core-gui -dit peerplays-core-gui
