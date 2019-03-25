c82dbe41c2c546217bc7c8658f8a5ec03f56bbb7
Ubuntu <ubuntu@ip-172-31-32-131.us-east-2.compute.internal>
2019-03-25 16:58:24 +0000

    Fixed docker process

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker stop peerplays-core-gui || true
 docker rm peerplays-core-gui || true
-docker build -t peerplays/core-gui ../
-docker run -p 8082:8082 --name peerplays-core-gui -dit peerplays/core-gui
+git clone -b develop https://github.com/RoshanSyed/peerplays-core-gui.git
+cd peerplays-core-gui
+docker build -t peerplays-core/gui .
+docker run -p 8082:8082 --name peerplays-core-gui -dit peerplays-core/gui
