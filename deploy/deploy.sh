cc40522d81c6348ada90735337dbe45964f8c158
Roshan Syed <r.syed@pbsa.info>
2019-03-27 16:31:50 -0300

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -32,5 +32,5 @@ echo "ALL_SERVERS ${ALL_SERVERS}"
 for server in "${ALL_SERVERS[@]}"
 do
   echo "deploying to ${server}"
-  ssh ubuntu@${server} 'bash' < $CI_COMMIT_REF_NAME ./deploy/updateAndRestart.sh
+  ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh $CI_COMMIT_REF_NAME
 done
