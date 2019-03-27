44896c35707bbb81d3bc031b6c4ae878f3743c61
Roshan Syed <r.syed@pbsa.info>
2019-03-27 16:24:06 -0300

    testing manual

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git clone -b $CI_COMMIT_REF_NAME https://github.com/RoshanSyed/peerplays-core-gui.git
+git clone -b $1 https://github.com/RoshanSyed/peerplays-core-gui.git
 cd peerplays-core-gui
 whoami
-pm2 delete peerplays-core-gui || true
+/home/ubuntu/.nvm/versions/node/v8.9.4/bin/pm2 delete peerplays-core-gui || true
 npm install
-pm2 start --name peerplays-core-gui npm -- start
+/home/ubuntu/.nvm/versions/node/v8.9.4/bin/pm2 start --name peerplays-core-gui npm -- start
