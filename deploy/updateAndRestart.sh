e36738fbf6073388803c7daaa27f0b25c823450b
Roshan Syed <r.syed@pbsa.info>
2019-03-27 14:54:25 -0300

    Testing new deploy

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker stop peerplays-core-gui || true
-docker rm peerplays-core-gui || true
+pm2 stop peerplays-core-gui || true
+pm2 delete peerplays-core-gui || true
 git clone -b develop https://github.com/RoshanSyed/peerplays-core-gui.git
 cd peerplays-core-gui
-docker build -t peerplays-core/gui .
-docker run -p 8082:8082 --name peerplays-core-gui -dit peerplays-core/gui
+npm install
+pm2 start --name peerplays-core-gui npm -- start
