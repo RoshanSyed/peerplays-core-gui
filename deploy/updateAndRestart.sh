c490801d7a479d17537965303133bc2a435f21f0
Roshan Syed <r.syed@pbsa.info>
2019-03-28 15:24:44 -0300

    test

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -4,5 +4,5 @@ git clone -b $ARG1 https://github.com/peerplays-network/peerplays-core-gui.git
 cd peerplays-core-gui
 /home/ubuntu/.nvm/versions/node/v8.9.4/bin/pm2 delete peerplays-core-gui || true
 npm install
-sh ../disableHostCheck
+sh ../disableHostCheck.sh
 /home/ubuntu/.nvm/versions/node/v8.9.4/bin/pm2 start --name peerplays-core-gui npm -- start
